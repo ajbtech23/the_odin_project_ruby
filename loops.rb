b9180a2c18219bfd9511b4b23add5147eefb79a4
ajbtech23 <joshuabanjo.tech@gmail.com>
2024-08-15 19:27:00 +0100

    Mickey Farrell

---
+++ loops.rb
@@ -0,0 +1,5 @@
+basketball_players = ['LeBron James', 'Steph Curry', 'Kevin Durant', 'Anthony Edwards', 'Bam Adebayo']
+
+for player in basketball_players
+    puts (player + ' is the greatest scorer of all time!')
+end
