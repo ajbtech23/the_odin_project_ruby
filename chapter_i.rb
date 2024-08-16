faba9d6bb93fdfd3fb315ffcb2b882b932b7ae85
ajbtech23 <joshuabanjo.tech@gmail.com>
2024-08-16 09:35:45 +0100

    Hmm Hmm

---
+++ chapter_i.rb
@@ -0,0 +1,12 @@
+nba_bucks = ['Giannis', 'Jrue', 'Middleton', 'Portis', 'DiVincenzo']
+nba_miami = ['Butler', 'Adebayo', 'Herro', 'Jovic', 'Rozier']
+
+puts 
+for player in nba_bucks
+    puts 'Welcome to the league ' + player
+end
+
+puts 
+for player in nba_miami
+    puts 'Welcome to the league ' + player
+end
---
+++ chapter_ii.rb
@@ -0,0 +1,9 @@
+puts((1+1).to_s)
+puts((2-1).to_s)
+puts((2*2).to_s)
+puts((10/5).to_s)
+puts((2**2).to_s)
+puts((3**4).to_s)
+puts((8%2).to_s)
+puts((10%4).to_s)
+
